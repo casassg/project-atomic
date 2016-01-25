acb347b809f8f7a1430c0b118a48d0bb0550a1ab
Gerard <casassg@gmail.com>
2016-01-25 10:57:30 +0100

    change endpoint to cities

--- www/js/services.js
+++ www/js/services.js
@@ -45,6 +45,51 @@ angular.module('starter.services', [])
         }
       }
       return null;
+    },
+    refresh: function() {
     }
   };
+})
+.factory('Cities', function($http) {
+    // Some fake testing data
+    var cities = [{
+      id: 0,
+      name: 'Barcelona',
+      lastText: 'You on your way?',
+      face: 'img/ben.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      face: 'img/max.png'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      face: 'img/adam.jpg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      face: 'img/perry.png'
+    }, {
+      id: 4,
+      name: 'Mike Harrington',
+      lastText: 'This is wicked good ice cream.',
+      face: 'img/mike.png'
+    }];
+
+
+
+  return {
+    get: function(city) {
+      return $http.get('http://api.openweathermap.org/data/2.5/weather?q=barcelona,es&APPID=155de3b370efd8e0f45c05494dfb08e9');
+    },
+    all: function() {
+      return cities;
+    },
+    refresh: function() {
+    }
+  }
+
 });
